9d93a3a408f552430754773f08812bc185af3309
KaiChen <237745537@qq.com>
2018-07-18 17:17:13 +0800

    optimize structure

--- src/main/java/com/jd/jdk8/lambda/LambdaListSort.java
+++ src/main/java/com/jd/jdk8/lambda/LambdaListSort.java
@@ -1,33 +1,56 @@
 package com.jd.jdk8.lambda;
 
+import com.jd.utils.GsonUtil;
 
-import org.junit.Test;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @Author:kaichenr
+ * @Date:2018/7/18 10:53
+ **/
 public class LambdaListSort {
 
-    @Test
-    public void lam1Test() {
-        Arrays.asList(1, 2, 3).forEach(e -> System.out.print(e));
-        Arrays.asList("a", "b", "c").forEach((String e) -> System.out.print(e));
-        Arrays.asList("a", "a", "c").sort((e1, e2) -> {
-            int result = e1.compareTo(e2);
-            System.out.println(result);
-            return result;
-        });
+    public static void main(String[] args) {
+        LambdaListSort lambdaListSort = new LambdaListSort();
+        lambdaListSort.sortList1();
+
     }
 
-    @Test
-    public void carTest() {
-        Car car1 = Car.create(Car::new);
-        Car car2 = Car.create(Car::new);
-        final List<Car> cars = Arrays.asList(car1, car2);
-        cars.forEach(Car::collide);
-        cars.forEach(Car::repair);
+    public void sortList1() {
+        List<Person> list1 = new ArrayList<Person>();
+        List<Person> list2 = new ArrayList<Person>();
 
-        final Car car3 = Car.create(Car::new);
-        System.out.println("car3:" + car3);
-        cars.forEach(car3::follow);
+        Person p1 = new Person("bob", 28);
+        Person p2 = new Person("tom", 19);
+        Person p3 = new Person("lilei", 16);
+        Person p4 = new Person("wolf", 21);
+        Person p5 = new Person("wolf2", 21);
+
+        list1.add(p1);
+        list1.add(p2);
+        list2.add(p3);
+        list2.add(p4);
+        list2.add(p5);
+        list1.addAll(list2);
+
+        list1.sort(
+                (o1, o2) -> {
+                    if (o1.getAge() == o2.getAge()) {
+                        return 0;
+                    }
+                    if (o1.getAge() > o2.getAge()) {
+                        return 1;
+                    } else {
+                        return -1;
+                    }
+                }
+        );
+
+        System.out.println(GsonUtil.GsonString(list1));
+    }
+
+    public void sortList2(){
+        
     }
 }
--- src/main/java/com/jd/jdk8/lambda/LambdaThread.java
+++ src/main/java/com/jd/jdk8/lambda/LambdaThread.java
@@ -5,11 +5,23 @@ package com.jd.jdk8.lambda;
  * @Date:2018/7/18 11:21
  **/
 public class LambdaThread {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
+        LambdaThread lt = new LambdaThread();
+        lt.oldThread();
     }
 
-    public void oldThread(){
+    public void oldThread() throws InterruptedException {
+        System.out.println("oldThread start....");
+        new Thread(
+                ()->{
+                    System.out.println("hello");
+                    System.out.println("hello world");
+                }
+        ).start();
+        System.out.println("oldThread end....");
+    }
 
+    public LambdaThread() {
+        super();
     }
 }
