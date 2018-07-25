445c1bc33ac91d86f59bf2fa705faeb9d45dd40f
KaiChen <237745537@qq.com>
2018-07-25 09:18:39 +0800

    enum

---
+++ src/main/java/com/jd/superior/datastructure/TestCase.java
@@ -0,0 +1,16 @@
+package com.jd.superior.datastructure;
+
+import org.junit.Test;
+
+/**
+ * @Author:kaichenr
+ * @Date:2018/7/24 17:43
+ * @Description:
+ **/
+public class TestCase {
+    @Test
+    public void testWeeks(){
+        System.out.println(Weeks.FriDay);
+        System.out.println(Weeks.FriDay);
+    }
+}
