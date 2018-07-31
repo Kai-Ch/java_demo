0000b6b6c8856e06adc8493dac519ae7a038bc0d
KaiChen <237745537@qq.com>
2018-07-31 10:14:53 +0800

    tree

--- src/main/java/com/jd/data/structure/NullTree.java
+++ src/main/java/com/jd/data/structure/NullTree.java
@@ -1,5 +1,7 @@
 package com.jd.data.structure;
 
+import com.jd.utils.GsonUtil;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,26 +25,86 @@ public class NullTree {
         this.data = data;
     }
 
-    public NullTree(Object data){
+    public NullTree(Object data) {
         this(null, null, data);
+    }
+
+    public NullTree() {
+        super();
+    }
+
+    public Object getData() {
+        return data;
     }
 
     /**
      * 创建树
      */
-    public void createTree(Object[] objects){
-        if(objects.length == 0){
+    public void createTree(Object[] objects) {
+        if (objects.length == 0) {
             return;
         }
         datas = new ArrayList<NullTree>();
-        for(Object o : objects){
+        for (Object o : objects) {
             datas.add(new NullTree(o));
         }
         root = datas.get(0);
 
-        for (int i = 0; i < datas.size()/2; i++) {
-
+        for (int i = 0; i < datas.size() / 2; i++) {
+            if (null != datas.get((i * 2 + 1))) {
+                datas.get(i).leftNode = datas.get(i * 2 + 1);
+            } else {
+                datas.get(i).leftNode = null;
+            }
+            if ((i * 2 + 2) < datas.size() && null != datas.get(i * 2 + 2)) {
+                datas.get(i).rightNode = datas.get(i * 2 + 2);
+            } else {
+                datas.get(i).rightNode = null;
+            }
         }
+    }
+
+    public NullTree getNullTree() {
+        return this;
+    }
+
+    public NullTree getRoot() {
+        return root;
+    }
+
+    public void setRoot(NullTree root) {
+        this.root = root;
+    }
+
+    /**
+     * 从节点开始遍历
+     */
+    public void preOrder(NullTree root) {
+        if (null != root && null != root.getData()) {
+            System.out.println(root.getData());
+            if (null != root.leftNode) {
+                System.out.print(" left: " + root.leftNode.getData());
+            } else {
+                System.out.print(" left: " + null);
+            }
+            if (null != root.rightNode) {
+                System.out.println(" right: " + root.rightNode.getData());
+            } else {
+                System.out.println(" right: " + null);
+            }
+            preOrder(root.leftNode);
+            preOrder(root.rightNode);
+        }
+    }
+
+    public static void main(String[] args) {
+        Object[] objs = {1, 2, null, 3, 4};
+        NullTree nullTree = new NullTree();
+        nullTree.createTree(objs);
+//        nullTree = nullTree.getNullTree();
+//        System.out.println(GsonUtil.GsonString(nullTree));
+
+        nullTree.preOrder(nullTree.getRoot());
     }
 
 }
