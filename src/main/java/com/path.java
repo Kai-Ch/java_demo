ad9230a56d998c100efe72819f058dae0c1711e0
KaiChen <237745537@qq.com>
2018-08-31 15:38:43 +0800

    commit

---
+++ src/main/java/com/path.java
@@ -0,0 +1,15 @@
+package com;
+
+import com.jd.utils.Log;
+
+/**
+ * @Author:kaichenr
+ * @Date:2018/8/31 15:30
+ * @Description:
+ **/
+public class path {
+    public static void main(String[] args) {
+        System.out.println("user.dir : " + System.getProperty("user.dir"));
+        System.out.println("webapp.root : " + System.getProperty("webapp.root"));
+    }
+}
