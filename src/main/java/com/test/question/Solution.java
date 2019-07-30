cce1b66c14f069f49f7d0b47cf7988899d59f93b
kaichenr <kaichenr@isoftstone.com>
2019-07-30 18:57:51 +0800

    commit

---
+++ src/main/java/com/test/question/Solution.java
@@ -0,0 +1,43 @@
+package com.test.question;
+
+import java.util.Arrays;
+
+public class Solution {
+    public boolean Find(int target, int [][] array) {
+        int row = array.length;
+        int col = array[0].length;
+        if (row == 0 || col == 0)
+            return false;
+        if (target < array[0][0] || target > array[row - 1][col - 1])
+            return false;
+        int i = 0;
+        int j = col - 1;
+        while (i < row && j >= 0)
+        {
+            if (array[i][j] > target)
+            {
+                System.out.println(array[i][j]);
+                j--;
+            }
+            else if (array[i][j] < target)
+            {
+                i++;
+            }
+
+            else
+            {
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int [][]  array = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        boolean r = solution.Find(3, array);
+        System.out.println(r);
+    }
+}
